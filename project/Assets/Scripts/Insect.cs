df7f14f2da5c674101c8d050bd743fb8284c670f
Specter <julien.dhenin@orange.fr>
2015-04-27 16:20:16 +0200

    Creation du projet + plusieurs behaviours

---
+++ project/Assets/Scripts/Insect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Insect : MonoBehaviour {
+
+	public int speed;
+	public string behaviour;
+
+	// Use this for initialization
+	void Start () {
+		SeekBehaviour seekBehaviour = GetComponent<SeekBehaviour>();
+		behaviour = "Wandering";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//randomMove ();
+		/*if(behaviour == "SeekBehaviour"){
+			seekBehaviour.
+		}*/
+	
+	}
+
+	/*public void randomMove(){
+
+		transform.Translate(1,0,0);
+		
+	}*/
+
+
+}
---
+++ project/Assets/Scripts/Worker.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Worker : MonoBehaviour {
+
+	public GameObject target;
+	public string behaviour;
+
+	// Use this for initialization
+	void Start () {
+		//findFood ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (behaviour == "GoToQueen") {
+			goToQueen ();
+		} else {
+			findFood ();
+		}
+
+
+		/*if (target.transform.position == this.transform.position) {
+			behaviour = "SearchFood";
+			findFood();
+		}*/
+	}
+
+	public void findFood(){
+		GameObject closestFood = GetClosestObject ("Food");
+		Debug.Log("Closest Object is " + closestFood.name);
+		this.target = closestFood;
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.03f);
+		if (target.transform.position == this.transform.position) {
+			Destroy(target);
+			behaviour = "GoToQueen";
+			goToQueen();
+		}
+	}
+
+	public void goToQueen (){
+		GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("Queen");
+		GameObject queen = objectsWithTag [0];
+		this.target = queen;
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.03f);
+	}
+
+	public GameObject GetClosestObject(string tag)
+	{
+		//find all gameObjects with tag "tag"
+		GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(tag);
+		GameObject closestObject = null;
+		foreach (GameObject obj in objectsWithTag)
+		{
+			if(!closestObject)
+			{
+				closestObject = obj;
+			}
+			//compares distances
+			if(Vector3.Distance(transform.position, obj.transform.position) <= Vector3.Distance(transform.position, closestObject.transform.position))
+			{
+				closestObject = obj;
+			}
+		}
+		return closestObject;
+	}
+}
