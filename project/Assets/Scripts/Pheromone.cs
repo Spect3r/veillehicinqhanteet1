e66387665974753fbf46ea1ebd8f1da9b787d01f
davia39 <anthony39@aliceadsl.fr>
2015-05-27 20:15:19 +0200

    Update Pheromone.cs

--- project/Assets/Scripts/Pheromone.cs
+++ project/Assets/Scripts/Pheromone.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Pheromone : MonoBehaviour {
 
 	private float timer = 0.0f;
+	private string message ="";
 
 	// Use this for initialization
 	void Start () {
@@ -16,4 +17,11 @@ public class Pheromone : MonoBehaviour {
 		if (timer >= 10.0f)
 			Destroy (gameObject);
 	}
+	public void setMessage(string m){
+		this.message = m;
+	}
+	public string getMessage(){
+		return this.message;
+	}
+
 }
